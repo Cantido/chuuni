072b77428346746c73de7cf6f74279eacf41c924
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-10-13 15:23:50 +0000

    Vendor assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,10 @@
 //     import "some-package"
 //
 
+import htmx from "../vendor/htmx"
+
+window.htmx = htmx
+
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
