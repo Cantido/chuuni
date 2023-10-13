8e4f8dfb81550826f59fcea6bc779a6c79500d56
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-10-13 15:44:49 +0000

    Add a little delay to the topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,7 +30,7 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("htmx:xhr:loadstart", _info => topbar.show())
+window.addEventListener("htmx:xhr:loadstart", _info => topbar.show(100))
 window.addEventListener("htmx:xhr:loadend", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
