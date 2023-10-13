feefd41764d37de7aa0833e3dbe471aeb4585162
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-10-13 15:39:38 +0000

    Get topbar working with HTMX

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,9 +30,8 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+window.addEventListener("htmx:xhr:loadstart", _info => topbar.show())
+window.addEventListener("htmx:xhr:loadend", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
