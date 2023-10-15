77211648c4c18e29d3149a24b6bc4a3f8eee186e
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-10-15 19:12:07 +0000

    Stub out actor outbox
    This needed a DB change because the ActivityPub code was expecting
    the object ID migration to exist. I should make a PR for this.

--- priv/repo/migrations/20231014211502_add_activity_pub.exs
+++ priv/repo/migrations/20231014211502_add_activity_pub.exs
@@ -1,5 +1,13 @@
 defmodule Chuuni.Repo.Migrations.AddActivityPub do
   use Ecto.Migration
 
-  def up, do: ActivityPub.Migrations.up()
+  def up do
+    ActivityPub.Migrations.up()
+    ActivityPub.Migrations.add_object_boolean()
+  end
+
+  def down do
+    ActivityPub.Migrations.drop_object_boolean()
+    ActivityPub.Migrations.down()
+  end
 end
