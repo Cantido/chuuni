844626849d01a213d28083cc5dbf29f00c52f266
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-12-29 16:51:25 -0700

    Fix front page card height

--- lib/chuuni_web/controllers/page_html.ex
+++ lib/chuuni_web/controllers/page_html.ex
@@ -13,47 +13,47 @@ defmodule ChuuniWeb.PageHTML do
 
   def anime_card(assigns) do
     ~H"""
-      <article class="card" aria-labelledby={"anime-card-#{@summary.anime.id}"}>
-        <a href={~p"/anime/#{@summary.anime}"}>
-          <div class="card-image">
+      <article class="card is-flex is-flex-direction-column" aria-labelledby={"anime-card-#{@summary.anime.id}"} style="height: 21em;">
+        <div class="card-image">
+          <a href={~p"/anime/#{@summary.anime}"}>
             <div class="image">
               <img
                 src={~p"/artwork/anime/#{@summary.anime}/cover.png"}
                 alt={@summary.anime.title.english}
               />
             </div>
-          </div>
+          </a>
+        </div>
 
-          <div class="is-size-7 is-flex is-flex-direction-column is-justify-content-space-between" style="height: 7rem; padding: 0.75rem;">
-            <header id={"anime-card-#{@summary.anime.id}"} style="overflow: hidden; max-height: 100%">
-              <%= @summary.anime.title.english %>
-            </header>
-            <div :if={@summary.count}>
-              <footer>
-                <span class="icon-text">
-                  <%= if @summary.rating do %>
-                    <%= Float.round(@summary.rating, 2) %>
-                  <% else %>
-                    <span aria-hidden="true">
-                      ??
-                    </span>
-                    <span class="is-sr-only">
-                      unknown
-                    </span>
-                  <% end %>
-                  <span class="icon">
-                    <img src={~p"/images/bootstrap/star-fill.svg"} alt="rating" />
-                  </span>
-                  &nbsp;
-                  <%= @summary.count %>
-                  <span class="icon ml-0">
-                    <img src={~p"/images/bootstrap/person-fill.svg"} alt="reviews" />
-                  </span>
-                </span>
-              </footer>
-            </div>
+        <div class="card-content is-size-7 is-flex is-flex-direction-column is-justify-content-space-between" style="padding: 0.75rem; flex-grow: 1">
+          <div id={"anime-card-#{@summary.anime.id}"} style="overflow: hidden;">
+            <%= @summary.anime.title.english %>
           </div>
-        </a>
+          <div :if={@summary.count}>
+            <footer>
+              <span class="icon-text">
+                <%= if @summary.rating do %>
+                  <%= Float.round(@summary.rating, 2) %>
+                <% else %>
+                  <span aria-hidden="true">
+                    ??
+                  </span>
+                  <span class="is-sr-only">
+                    unknown
+                  </span>
+                <% end %>
+                <span class="icon">
+                  <img src={~p"/images/bootstrap/star-fill.svg"} alt="rating" />
+                </span>
+                &nbsp;
+                <%= @summary.count %>
+                <span class="icon ml-0">
+                  <img src={~p"/images/bootstrap/person-fill.svg"} alt="reviews" />
+                </span>
+              </span>
+            </footer>
+          </div>
+        </div>
       </article>
     """
   end
