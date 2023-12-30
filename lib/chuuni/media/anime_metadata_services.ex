69f6e596e7a5fd2a8f53bef757cad9d7a9a5c7d7
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-12-30 13:42:31 -0700

    Add MAL IDs to pages

--- lib/chuuni/media/anime_metadata_services.ex
+++ lib/chuuni/media/anime_metadata_services.ex
@@ -4,12 +4,14 @@ defmodule Chuuni.Media.AnimeMetadataServices do
 
   embedded_schema do
     field :anilist, :string
+    field :myanimelist, :string
   end
 
   @doc false
   def changeset(%AnimeMetadataServices{} = anime_metadata_services, attrs) do
     anime_metadata_services
-    |> cast(attrs, [:anilist])
+    |> cast(attrs, [:anilist, :myanimelist])
     |> validate_format(:anilist, ~r/\d{1,9}/)
+    |> validate_format(:myanimelist, ~r/\d{1,9}/)
   end
 end
